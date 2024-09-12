14c77f27172988162e5a40168327ca0390459bce
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-09-12 10:01:42 +0200

    task 2

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,17 +1,11 @@
-###
-#
-#  Sort integer arguments (ascending) 
-#
-###
-
 result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next unless arg !~ /^-?[0-9]+$/
 
-    result >> arg.to_i
+ARGV.each do |arg|
+    next unless arg =~ /^-?[0-9]+$/
+
+    result << arg.to_i
 end
 
-result.Sort!
+result.sort!
 
 puts result
